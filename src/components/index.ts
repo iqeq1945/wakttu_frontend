aff36eaabcc73b8179055241ac3927b299e2c30a
ccconac <qwerpoiu91@naver.com>
2024-05-11 15:27:38 +0900

    chore: add component module export

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as AuthForm } from './auth/AuthForm';
+export { default as AuthInput } from './auth/AuthInput';
+export { default as AuthButton } from './auth/AuthButton';
+export { default as TemporaryLayout } from './Layout/TemporaryLayout';
+export { default as LoginedMain } from './main/Logined';
