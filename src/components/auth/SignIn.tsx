85b0b7d7c2d4dc5a852308fc5f483f85fd2e1396
ccconac <qwerpoiu91@naver.com>
2024-05-13 01:01:53 +0900

    feat: implement user sign in

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,15 +1,78 @@
-import { ChangeEvent } from 'react';
-import { AuthForm, AuthInput, AuthButton } from '@/components/index';
+import axios from 'axios';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
-interface Props {
+import { API_URL } from '@/services/api';
+import { ERROR_MESSAGE } from '@/constants/auth';
+import { AuthForm, AuthInput, AuthButton } from '@/components/index';
+import { isExistError, isIdValidError } from '@/containers/auth/checkAuth';
+
+interface SignInProps {
   id: string;
   pw: string;
   onInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const SignIn = ({ id, pw, onInputChange }: Props) => {
+interface ErrorProps {
+  message: string;
+  type: string;
+}
+
+const SignIn = ({ id, pw, onInputChange }: SignInProps) => {
+  const [errors, setErrors] = useState<ErrorProps>();
+
+  const isIdValid = async (userId: string) => {
+    let sameId = true;
+
+    try {
+      const { data } = await axios.post(`${API_URL}/auth/check/id`, { id: userId });
+      sameId = data.success;
+    } catch (error) {
+      if (axios.isAxiosError(error)) sameId = error?.response?.data.success;
+    }
+
+    return sameId;
+  };
+
+  const onSignInSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const idValid = await isIdValid(id);
+
+    if (isExistError(id, pw)) {
+      setErrors(isExistError(id, pw));
+      return;
+    }
+
+    if (isIdValidError(idValid)) {
+      setErrors(isIdValidError(idValid));
+      return;
+    }
+
+    const userInfo = {
+      email: id,
+      password: pw,
+    };
+
+    await axios
+      .post(`${API_URL}/auth/login`, userInfo)
+      .then((response) => {
+        if (response.status === 201) {
+          setErrors({ message: '', type: 'success' });
+          console.log('로그인 완료!');
+        }
+      })
+      .catch((error) => {
+        setErrors({
+          message: ERROR_MESSAGE.signInError,
+          type: 'pw',
+        });
+
+        console.error(`로그인을 완료할 수 없습니다. ${error}`);
+      });
+  };
+
   return (
-    <AuthForm formTitle="로그인">
+    <AuthForm formTitle="로그인" onSubmit={onSignInSubmit}>
       <AuthInput type="text" placeholder="아이디" name="id" value={id} onChange={onInputChange} />
       <AuthInput
         type="password"
@@ -18,6 +81,7 @@ const SignIn = ({ id, pw, onInputChange }: Props) => {
         value={pw}
         onChange={onInputChange}
       />
+      {errors && <span>{errors.message}</span>}
       <AuthButton buttonText="로그인" />
     </AuthForm>
   );
