c255d578a9680100d5d075de5701d5e709d54ef2
ccconac <qwerpoiu91@naver.com>
2024-05-12 15:17:18 +0900

    feat: implement input custom hook

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,18 @@
+import { useState, ChangeEvent } from 'react';
+
+const useInput = <T>(initInputs: T) => {
+  const [inputs, setInputs] = useState(initInputs);
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  return { inputs, setInputs, onInputChange };
+};
+
+export default useInput;
