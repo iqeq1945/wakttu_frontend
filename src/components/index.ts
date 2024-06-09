65f7cf0ec220b83e5b221b0ef9bfb67052fdbf06
ccconac <qwerpoiu91@naver.com>
2024-06-09 15:15:41 +0900

    :truck: rename: rename Layout directory

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ export { default as AuthForm } from './auth/AuthForm';
 export { default as AuthInput } from './auth/AuthInput';
 export { default as AuthButton } from './auth/AuthButton';
 
-export { default as TemporaryLayout } from './Layout/TemporaryLayout';
+export { default as TemporaryLayout } from './layout/TemporaryLayout';
 export { default as LoginedMain } from './main/Logined';
