0f30bcd3ab05ad606e4ed93d4710faea573adf69
ccconac <qwerpoiu91@naver.com>
2024-06-09 15:17:58 +0900

    :sparkles: feat: connect socket upon login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Auth from '@/containers/auth/Auth';
+import SocketManager from '@/services/socketManager';
 import { selectUserId } from '@/redux/user/userSlice';
 import { LoginedMain } from '@/components/index';
 
@@ -11,8 +12,15 @@ const Main = () => {
 
   useEffect(() => {
     if (userId) setIsLogined(true);
+    if (!userId) setIsLogined(false);
   }, [userId]);
-  return <div>{isLogined ? <LoginedMain isLogined={isLogined} /> : <Auth />}</div>;
+
+  return (
+    <div>
+      <SocketManager />
+      {isLogined ? <LoginedMain isLogined={isLogined} /> : <Auth />}
+    </div>
+  );
 };
 
 export default Main;
