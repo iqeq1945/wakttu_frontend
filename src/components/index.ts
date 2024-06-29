a6b9620cbebf49efcd3b0398b55f85976e5b394c
ccconac <qwerpoiu91@naver.com>
2024-06-29 23:37:14 +0900

    :truck: move: move tab component to common directory

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,4 +5,4 @@ export { default as AuthButton } from './auth/AuthButton';
 export { default as TemporaryLayout } from './layout/TemporaryLayout';
 export { default as LoginedMain } from './main/Logined';
 
-export { default as Tab } from './roomList/Tab';
+export { default as Tab } from './common/Tab';
