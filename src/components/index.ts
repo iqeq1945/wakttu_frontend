15f7ef6195cda595861de2cbbb16d8bcbab4a205
Ungs <iqeq1945@gmail.com>
2024-08-26 19:44:14 +0900

    fix : test code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as AuthForm } from './auth/AuthForm';
 export { default as AuthInput } from './auth/AuthInput';
 
-export { default as TemporaryLayout } from './layout/TemporaryLayout';
 export { default as MainHeader } from './main/Header';
 export { default as MainTab } from './main/Tab';
 export { default as MainForm } from './main/MainForm';
