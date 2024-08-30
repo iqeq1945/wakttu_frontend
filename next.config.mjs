0075b4f542702acc79744b92b8303206fbffd723
kms0219kms <kms0219kms@naver.com>
2024-08-30 20:27:42 +0700

    :sparkles: feat: Add Soundplayer hook and fix FOUC of styled-components

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: false,
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
