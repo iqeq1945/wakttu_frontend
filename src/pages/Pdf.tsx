637f99322fcd970272f9eb7d3f0902e26861d5ee
Ungs <iqeq1945@gmail.com>
2024-09-27 21:01:30 +0900

    fix :  Pdf.tsx
    error code 수정

--- src/pages/Pdf.tsx
+++ src/pages/Pdf.tsx
@@ -1,5 +1,5 @@
 import { PdfViewer } from '@naverpay/react-pdf';
-import '@naverpay/react-pdf/index.css'
+//import '@naverpay/react-pdf/index.css'
 
 const PDF = () => {
   return <PdfViewer pdfUrl={`${process.env.NEXT_PUBLIC_R2_URL}/wakttu.pdf`} />;
