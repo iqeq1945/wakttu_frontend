7f7830317a45de8af6c6c2c51329332f1e8ffba6
Minsu Kim <minsu_kim@bishanoi.net>
2024-09-27 17:04:18 +0700

    :bug: fix: PDF rendering issue - temporary fix

--- src/pages/Pdf.tsx
+++ src/pages/Pdf.tsx
@@ -1,7 +1,8 @@
 import { PdfViewer } from '@naverpay/react-pdf';
+import '@naverpay/react-pdf/index.css'
 
 const PDF = () => {
-  return <PdfViewer pdfUrl={'https://r2.wakttu.kr/wakttu.pdf'} />;
+  return <PdfViewer pdfUrl={`${process.env.NEXT_PUBLIC_R2_URL}/wakttu.pdf`} />;
 };
 
 export default PDF;
