76b862c7306a76a3f3a651c65d5351fa40b5be78
atik-persei <atikpersei@gmail.com>
2024-09-30 18:02:08 +0000

    ✨ feat: Add utility function for matching English and Korean names
    - Implemented a utility function to match English and Korean names

---
+++ src/utils/matchENtoKR.ts
@@ -0,0 +1,14 @@
+import { RelevantPerson } from "@/styles/dictionary/Word";
+
+export const matchENtoKR = (relevantPerson: RelevantPerson) => {
+  const koreanNames = {
+    "woowakgood": "우왁굳",
+    "ine": "아이네",
+    "jingburger": "징버거",
+    "lilpa": "릴파",
+    "jururu": "주르르",
+    "gosegu": "고세구",
+    "viichan": "비챤"
+  };
+  return koreanNames[relevantPerson];
+}
