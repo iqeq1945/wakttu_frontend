41cda52873822eb7b17a5075b4750d705bf9f75a
Ungs <iqeq1945@gmail.com>
2024-10-06 19:10:28 +0900

    fix : error page

--- src/pages/test.tsx
+++
@@ -1,7 +0,0 @@
-import { client, updatePlayCount, updateResult } from '@/services/api';
-import Result from '@/components/room/Result';
-const Test = () => {
-  return <Result />;
-};
-
-export default Test;
