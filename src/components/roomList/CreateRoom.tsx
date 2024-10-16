8cf8e58dc540a6d2cadda2266ef857ac9ee0c8f0
Ungs <iqeq1945@gmail.com>
2024-10-16 20:16:03 +0900

    fix : code error

--- src/components/roomList/CreateRoom.tsx
+++ src/components/roomList/CreateRoom.tsx
@@ -99,6 +99,9 @@ const CreateRoom = ({
                 <DropdownItem onClick={() => onSelect('type', 1)}>
                   쿵쿵따
                 </DropdownItem>
+                <DropdownItem onClick={() => onSelect('type', 2)}>
+                  왁타골든벨
+                </DropdownItem>
               </>
             )}
           </Dropdown>
@@ -112,68 +115,77 @@ const CreateRoom = ({
             onChange={onRoomInfo}
             min={roomInfo.type === 2 ? '10' : '3'}
             max={roomInfo.type === 2 ? '30' : '8'}
-
           />
         </CCreate>
-        <CCreate>
-          <CLabel>라운드시간</CLabel>
-          <Dropdown onClick={() => onDropdown(1)}>
-            <Selected>{roomInfo.time / 1000}초</Selected>
-            <DropdownLine
-              isopen={isDown[1]}
-              src={getR2URL('/assets/icons/down-line.svg')}
-            />
-            {isDown[1] && (
-              <>
-                <DropdownItem onClick={() => onSelect('time', 30000)}>
-                  30초
-                </DropdownItem>
-                <DropdownItem onClick={() => onSelect('time', 60000)}>
-                  60초
-                </DropdownItem>
-                <DropdownItem onClick={() => onSelect('time', 90000)}>
-                  90초
-                </DropdownItem>
-                <DropdownItem onClick={() => onSelect('time', 120000)}>
-                  120초
-                </DropdownItem>
-              </>
-            )}
-          </Dropdown>
-        </CCreate>
-        <CCreate>
-          <CLabel>특수규칙</CLabel>
-          <CheckBox onClick={() => onSelect('option', '팀전')}>
-            <CCheck>
-              {roomInfo.option.indexOf('팀전') === -1 ? (
-                <CheckIcon src={getR2URL('/assets/icons/check-off.svg')} />
-              ) : (
-                <CheckIcon src={getR2URL('/assets/icons/check-on.svg')} />
-              )}
-              <Selected tooltip='팀전이 가능해 집니다. 총 4팀까지 나눌 수 있습니다.'>팀전</Selected>
-            </CCheck>
-          </CheckBox>
-          <CheckBox onClick={() => onSelect('option', '매너')}>
-            <CCheck>
-              {roomInfo.option.indexOf('매너') === -1 ? (
-                <CheckIcon src={getR2URL('/assets/icons/check-off.svg')} />
-              ) : (
-                <CheckIcon src={getR2URL('/assets/icons/check-on.svg')} />
-              )}
-              <Selected tooltip='한방 단어를 사용 할 수 없게 됩니다.'>매너</Selected>
-            </CCheck>
-          </CheckBox>
-          <CheckBox onClick={() => onSelect('option', '외수')}>
-            <CCheck>
-              {roomInfo.option.indexOf('외수') === -1 ? (
-                <CheckIcon src={getR2URL('/assets/icons/check-off.svg')} />
-              ) : (
-                <CheckIcon src={getR2URL('/assets/icons/check-on.svg')} />
-              )}
-              <Selected tooltip='끄투의 어인정 단어를 사용할 수 있게 됩니다.'>외수</Selected>
-            </CCheck>
-          </CheckBox>
-        </CCreate>
+        {roomInfo.type !== 2 && (
+          <>
+            <CCreate>
+              <CLabel>라운드시간</CLabel>
+              <Dropdown onClick={() => onDropdown(1)}>
+                <Selected>{roomInfo.time! / 1000}초</Selected>
+                <DropdownLine
+                  isopen={isDown[1]}
+                  src={getR2URL('/assets/icons/down-line.svg')}
+                />
+                {isDown[1] && (
+                  <>
+                    <DropdownItem onClick={() => onSelect('time', 30000)}>
+                      30초
+                    </DropdownItem>
+                    <DropdownItem onClick={() => onSelect('time', 60000)}>
+                      60초
+                    </DropdownItem>
+                    <DropdownItem onClick={() => onSelect('time', 90000)}>
+                      90초
+                    </DropdownItem>
+                    <DropdownItem onClick={() => onSelect('time', 120000)}>
+                      120초
+                    </DropdownItem>
+                  </>
+                )}
+              </Dropdown>
+            </CCreate>
+            <CCreate>
+              <CLabel>특수규칙</CLabel>
+              <CheckBox onClick={() => onSelect('option', '팀전')}>
+                <CCheck>
+                  {roomInfo.option!.indexOf('팀전') === -1 ? (
+                    <CheckIcon src={getR2URL('/assets/icons/check-off.svg')} />
+                  ) : (
+                    <CheckIcon src={getR2URL('/assets/icons/check-on.svg')} />
+                  )}
+                  <Selected tooltip="팀전이 가능해 집니다. 총 4팀까지 나눌 수 있습니다.">
+                    팀전
+                  </Selected>
+                </CCheck>
+              </CheckBox>
+              <CheckBox onClick={() => onSelect('option', '매너')}>
+                <CCheck>
+                  {roomInfo.option!.indexOf('매너') === -1 ? (
+                    <CheckIcon src={getR2URL('/assets/icons/check-off.svg')} />
+                  ) : (
+                    <CheckIcon src={getR2URL('/assets/icons/check-on.svg')} />
+                  )}
+                  <Selected tooltip="한방 단어를 사용 할 수 없게 됩니다.">
+                    매너
+                  </Selected>
+                </CCheck>
+              </CheckBox>
+              <CheckBox onClick={() => onSelect('option', '외수')}>
+                <CCheck>
+                  {roomInfo.option!.indexOf('외수') === -1 ? (
+                    <CheckIcon src={getR2URL('/assets/icons/check-off.svg')} />
+                  ) : (
+                    <CheckIcon src={getR2URL('/assets/icons/check-on.svg')} />
+                  )}
+                  <Selected tooltip="끄투의 어인정 단어를 사용할 수 있게 됩니다.">
+                    외수
+                  </Selected>
+                </CCheck>
+              </CheckBox>
+            </CCreate>
+          </>
+        )}
         <CButton>
           <ConfirmButton type="button" onClick={onCreate}>
             <ButtonText>방 만들기</ButtonText>
