2bd0e1f2daa892e7f9258eb41e4fcfd220b15d76
Minsu Kim <minsu_kim@bishanoi.net>
2024-11-16 00:36:39 +0700

    :pencil2: typo(Info): 약관명 수정

--- src/components/main/Info.tsx
+++ src/components/main/Info.tsx
@@ -6,19 +6,19 @@ const WakttuInfo = () => {
   return (
     <Info>
       <>
-        <CopyRight>© copyright WAKTTU.</CopyRight>
+        <CopyRight>© 2024 WAKTTU</CopyRight>
         <CopyRight>
           왁뚜는 왁타버스에서 제공하는 공식 콘텐츠가 아닙니다.
         </CopyRight>
         <Flex>
           <Pdf onClick={() => window.open('/info/privacy', '_blank')}>
-            개인정보처리방침
+            개인정보 처리방침
           </Pdf>
           <Pdf onClick={() => window.open('/info/service', '_blank')}>
-            서비스이용방침
+            서비스 이용약관
           </Pdf>
 
-          <CopyRight>문의 : admin@wakttu.kr</CopyRight>
+          <CopyRight onClick={() => location.href = 'mailto:admin@wakttu.kr'}>문의 : admin@wakttu.kr</CopyRight>
         </Flex>
       </>
     </Info>
