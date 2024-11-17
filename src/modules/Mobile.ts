e3f9affa54ad7c8f1a98d0582ed9576b6838c0d4
Minsu Kim <minsu_kim@bishanoi.net>
2024-11-17 12:33:20 +0700

    :bug: fix(Mobile): 모바일 대응 width 수정

--- src/modules/Mobile.ts
+++ src/modules/Mobile.ts
@@ -2,5 +2,5 @@ export const isMobileDevice = (): boolean => {
     //const userAgent = window.navigator.userAgent;
     //return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 
-    return window.matchMedia("(max-width: 1440px)").matches;
+    return window.matchMedia("(max-width: 767px)").matches;
 }
