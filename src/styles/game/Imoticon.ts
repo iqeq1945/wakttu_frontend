bb1898961d688300065253c29bf6553b9745ce8b
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-11-19 22:55:32 +0900

    design: Implementation of in-game emoticon design

---
+++ src/styles/game/Imoticon.ts
@@ -0,0 +1,32 @@
+import styled, { keyframes } from 'styled-components';
+
+const growShrink = keyframes`
+  0% {
+    transform: scale(0);
+  }
+  20% {
+    transform: scale(1);
+  }
+  80% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+`;
+
+export const ImoticonImage = styled.img`
+  position: absolute;
+  top: -2.8125rem;
+  right: -2.8125rem;
+
+  width: 6.25rem;
+  height: 6.25rem;
+
+  z-index: 2;
+
+  object-fit: cover;
+
+  transform: scale(0);
+  animation: ${growShrink} 2s ease-in-out;
+`;
